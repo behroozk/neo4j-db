824e069769c72b191a6e88cc5cd165c5528880a4
Behrooz Kamali <behroozkamali@yahoo.com>
2019-03-17 22:06:49 -0400

    improve fetching result by parsing streamed parts before all arive

--- src/bolt/bolt_session.ts
+++ src/bolt/bolt_session.ts
@@ -1,21 +1,54 @@
+import * as Logger from "console";
 import { v1 as Neo4j } from "neo4j-driver";
 
+import { parseNeo4jResult } from "../parse_result";
 import { IQueryOptions } from "../types/query_options.interface";
 import { INeo4jSession, INeo4jSessionOptions } from "../types/session.interface";
-import { AbstractBoltSession } from "./bolt_abstract";
 
-export class Neo4jBoltSession extends AbstractBoltSession implements INeo4jSession {
-    constructor(private session: Neo4j.Session, private options: INeo4jSessionOptions) {
-        super();
-    }
+export class Neo4jBoltSession implements INeo4jSession {
+    constructor(private session: Neo4j.Session, private options: INeo4jSessionOptions) { }
 
-    public async execute(query: string, options: IQueryOptions = {}): Promise<any> {
-        const result = await this.session.run(query);
-        this.session.close();
+    public execute(query: string, options: IQueryOptions = {}): Promise<any> {
+        return new Promise((resolve, reject) => {
+            const parsedRecords: any[] = [];
+            let totalParseTime = 0;
+            this.session.run(query).subscribe({
+                onCompleted: () => {
+                    Logger.log(`Total parse time: ${totalParseTime}ms`);
+                    if (options.singularOutput) {
+                        resolve(parsedRecords[0]);
+                    } else {
+                        resolve(parsedRecords);
+                    }
 
-        return this.parseRecords(result.records, {
-            singularOutput: options.singularOutput,
-            stringFormatter: this.options.stringFormatter,
+                    this.session.close();
+                },
+                onError: (error) => {
+                    reject(error);
+                    this.session.close();
+                },
+                onNext: (record) => {
+                    const startTime = Date.now();
+                    const parsedRecord = parseNeo4jResult(record, this.options.stringFormatter);
+                    totalParseTime += (Date.now() - startTime);
+
+                    if (options.singularOutput && parsedRecords.length > 1) {
+                        reject(new Error(`multiple records returned in signular mode`));
+                        return this.session.close();
+                    }
+
+                    parsedRecords.push(parsedRecord);
+                },
+            });
         });
+
+        // promise approach to get all results at once
+        // const result = await this.session.run(query);
+        // this.session.close();
+
+        // return this.parseRecords(result.records, {
+        //     singularOutput: options.singularOutput,
+        //     stringFormatter: this.options.stringFormatter,
+        // });
     }
 }
